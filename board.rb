0bac01c1ad6d31eca4662169ecd76376ea5f8cf0
RyanAndWill <AppAcademy.io>
2015-10-02 14:18:00 -0400

    Add live and die methods.

--- board.rb
+++ board.rb
@@ -1,38 +1,57 @@
 require_relative "tile"
 
+
 class Board
+  attr_reader :mines_count, :grid, :dim
+
   def initialize(dim = 10)
     @dim = dim
-    @mine_count = dim
+    @mines_count = dim
     @grid = Array.new(dim) {Array.new(dim)}
+    @dead = false
   end
 
   def place_mines
-    mines_arr = mine_count * [ true ]
-    remaining_tiles = dim ** 2 - mines_arr
+    mines_arr = [ true ] * mines_count
+    remaining_tiles = dim ** 2 - mines_count
 
-    mines_arr += remaining_tiles * [false]
+    mines_arr += [false] * remaining_tiles
     mines_arr.shuffle
 
     (0...dim).each do |row|
       (0...dim).each do |col|
-        self[row, col] = Tile.new(self, [row, col], mines_arr.pop)
+        self[[row, col]] = Tile.new(self, [row, col], mines_arr.pop)
       end
     end
   end
 
   def [](pos)
     x, y = pos
-    @grid[x][y]
+    grid[x][y]
   end
 
   def []=(pos, value)
     x,y = pos
-    @grid[x][y] = value
+    grid[x][y] = value
   end
 
   def game_over?
-    won? || die?
+    live? || dead
   end
 
+  def live?
+    grid.flatten.all? do |tile|
+    end
+
+  end
+
+  def die(tile)
+    dead = true if tile.mine && tile.revealed
+  end
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  b = Board.new
 end
