7c37c4ca845504244e99badc97f2e932848fcdd6
RyanAndWill <AppAcademy.io>
2015-10-02 12:19:50 -0400

    Place Mines

--- board.rb
+++ board.rb
@@ -8,7 +8,27 @@ class Board
   end
 
   def place_mines
-    testp
+    mines_arr = mine_count * [ true ]
+    remaining_tiles = dim ** 2 - mines_arr
+
+    mines_arr += remaining_tiles * [false]
+    mines_arr.shuffle
+
+    (0...dim).each do |row|
+      (0...dim).each do |col|
+        self[row, col] = Tile.new(self, mines_arr.pop)
+      end
+    end
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos, value)
+    x,y = pos
+    @grid[x][y] = value
   end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,9 +1,19 @@
 class Tile
 
-  def initialize(mine = false)
+  def initialize(board, mine = false)
     @mine = mine
     @flagged = false
     @revealed = false
+    @board = board
   end
-  
+
+  #new arr = bomb_count [true]
+
+  def reveal
+    @revealed = true
+  end
+
+  def neighbors
+  end
+
 end
